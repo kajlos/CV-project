78a6418aa647f98ef56b26705a5417e836a0afa2
Kajetan <kajetan.loska@op.pl>
2022-08-12 17:39:21 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
